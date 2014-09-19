836f0d15486721cd161cb40225a671b354ce16c7
Mark de Jong <mark@markdejong.com>
2014-09-19 17:53:13 +0200

    Add deps. and ignore files
    Added Mongoose
    Added sublime project files to gitignore
    Changed express.favicon for serve-favicon

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 var express = require('express'),
+    favicon = require('serve-favicon'),
+    mongoose = require('mongoose'),
     app = express(),
     bodyParser = require('body-parser');
 
 var base = '/api',
     port = process.env.PORT || 5123;
 
-// app.use(express.bodyParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.favicon());
+var Todo = require('./app/models/todos');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(favicon(__dirname + '/static/favicon.ico'));
+
+
+// var quotes = [
+//   { author: 'Audrey Hepburn', text: "Nothing is impossible, the word itself says 'I'm possible'!" },
+//   { author: 'Walt Disney', text: "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you" },
+//   { author: 'Unknown', text: "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+//   { author: 'Neale Donald Walsch', text: "You are afraid to die, and you're afraid to live. What a way to exist."}
+// ];
 
 // MongoHQ db: mongodb://dappersome:,d4pp3rs0m3.@kahana.mongohq.com:10053/ds_todoist
-var quotes = [
-  { author: 'Audrey Hepburn', text: "Nothing is impossible, the word itself says 'I'm possible'!" },
-  { author: 'Walt Disney', text: "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you" },
-  { author: 'Unknown', text: "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-  { author: 'Neale Donald Walsch', text: "You are afraid to die, and you're afraid to live. What a way to exist."}
-];
+// Local db: mongodb://@localhost:27017/ds_todoist
+mongoose.connect('mongodb://@localhost:27017/ds_todoist');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -27,18 +33,23 @@ app.use(bodyParser.json());
 // =============================================================================
 // var router = express.Router();        // get an instance of the express Router
 
+app.use(function(req, res, next) {
+  console.log('%s %s - %s', (new Date).toString(), req.method, req.url);
+  return next();
+});
+
+
 app.get('/', function(req, res) {
   res.json(quotes);
 });
 
-app.get('/quote/random', function(req, res) {
-  var id = Math.floor(Math.random() * quotes.length);
-  var q = quotes[id];
-  res.json(q);
-});
+// app.get('/todo/random', function(req, res) {
+//   var id = Math.floor(Math.random() * quotes.length);
+//   var q = quotes[id];
+//   res.json(q);
+// });
 
-
-app.get('/quote/:id', function(req, res) {
+app.get('/todo/:id', function(req, res) {
   if (quotes.length <= req.params.id || req.params.id < 0) {
     res.statusCode = 404;
     return res.send('Error 404: No quote found');
@@ -47,23 +58,25 @@ app.get('/quote/:id', function(req, res) {
   res.json(q);
 });
 
-app.post('/quote', function(req, res) {
-  if(!req.body.hasOwnProperty('author') || 
-     !req.body.hasOwnProperty('text')) {
+
+app.post('/todo', function(req, res) {
+  if(!req.body.hasOwnProperty('title')) {
+
     res.statusCode = 400;
     return res.send('Error 400: Post syntax incorrect.');
+
   } 
  
-var newQuote = {
-    author : req.body.author,
-    text : req.body.text
+  var newTodo = {
+    title : req.body.title,
+    id : (new Date).getTime()
   }; 
  
-quotes.push(newQuote);
+  quotes.push(newTodo);
   res.json(true);
 });
 
-app.delete('/quote/:id', function(req, res) {
+app.delete('/todo/:id', function(req, res) {
   if(quotes.length <= req.params.id) {
     res.statusCode = 404;
     return res.send('Error 404: No quote found');
